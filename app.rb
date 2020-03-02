8068fac4ae58502adb2c820ab432231312217d85
Khalid T <khalid.tissire@gmail.com>
2020-03-02 10:24:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
-      "<h1>Hello World</h1>"
+	get '/' do
+		erb :index  
+	end
+
+  get '/info' do 
+     erb :info
   end
 end
